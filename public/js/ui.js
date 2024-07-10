be28d62c8cbb4f108493fe167d9b4f6a99d00ead
Vitin-Ps <vicssoares2802@gmail.com>
2024-07-10 17:48:24 -0300

    configurações vercel 2

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,5 +1,6 @@
 import * as constants from './constants.js';
 import * as elements from './elements.js';
+module.exports
 
 export const atualizaCodUnico = (codUnico) => {
   const cod_unico_valor = document.getElementById('cod_unico_valor');

4a8e81edac053f9b9437b90ef13248977030342f
Vitin-Ps <vicssoares2802@gmail.com>
2024-07-10 13:00:57 -0300

    configurações vercel

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import http from 'http';
 import { Socket } from 'socket.io';
+import path from 'path';
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,11 +9,19 @@ const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
+// app.get('/', (req: Request, res: Response) => {
+//   //   res.sendFile(__dirname + 'public/index.html');
+//   res.sendFile(__dirname + '/public/index.html');
+// });
+
+// Configuração para servir arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota principal que envia o arquivo 'index.html'
 app.get('/', (req: Request, res: Response) => {
-  //   res.sendFile(__dirname + 'public/index.html');
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 let peersConectados: String[] = [];

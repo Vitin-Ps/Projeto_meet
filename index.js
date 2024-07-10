31701788da164070fc090ebaf1e50456b4cbc0cb
Vitin-Ps <vicssoares2802@gmail.com>
2024-07-10 17:36:54 -0300

    configurações vercel

--- index.js
+++ index.js
@@ -8,13 +8,11 @@ const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
-// app.use(express.static('public'));
+// Servir arquivos estáticos da pasta public
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-  // res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 let peersConmectados = [];
